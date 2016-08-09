fd377201b0213d0c6750b8a99ca440ae160d4a6a
Morven Lewis-Everley <morven@i-lateral.com>
2016-08-09 10:41:06 +0100

    Update admin URL and title

--- code/admin/SystemMessageAdmin.php
+++ code/admin/SystemMessageAdmin.php
@@ -6,6 +6,12 @@
  */
 class SystemMessageAdmin extends ModelAdmin
 {
+    private static $url_segment = 'systemmessages';
+
+    private static $menu_title = 'System Messages';
+
+    private static $menu_priority = 5;
+    
     private static $managed_models = array(
         "SystemMessage"
     );
