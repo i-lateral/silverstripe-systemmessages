8b57834acde1fb2584f51cc331d49df22e5c7d7d
Makreig <mark@i-lateral.com>
2020-03-25 13:22:23 +0000

    added extensions for subsites

--- _config.php
+++ _config.php
@@ -1,1 +1,7 @@
 <?php
+
+// Enable Subsite Support if needed
+if(class_exists('Subsite')) {
+    SystemMessageAdmin::add_extension('SubsiteMenuExtension');
+    SystemMessage::add_extension("SystemMessage_SubsiteExtension");
+}
